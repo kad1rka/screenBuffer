910c35b90ba7c3548b8b93074bb5adb1e4f285fe
Nikita Rubtsov <118419926+kad1rka@users.noreply.github.com>
2024-10-11 09:48:03 +0300

    add OutOnScreen function

--- screenBuffer/screenBuffer.cpp
+++ screenBuffer/screenBuffer.cpp
@@ -61,11 +61,35 @@ int bufferCreationActivation() {
 }
 
 
+int OutOnScreen() {
+    // 2. Вывод на экран информации о буфере экрана
+    HANDLE hStdOut;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    if (!GetConsoleScreenBufferInfo(hStdOut, &csbi))
+        std::cout << "Console screen buffer info failed.\n";
+    std::cout << "Console screen buffer info: " << std::endl << std::endl;
+
+    std::cout << "A number of columns = " << csbi.dwSize.X << std::endl;
+    std::cout << "A number of rows = " << csbi.dwSize.Y << std::endl;
+    std::cout << "X cursor coordinate = " << csbi.dwCursorPosition.X << std::endl;
+    std::cout << "Y cursor coordinate = " << csbi.dwCursorPosition.Y << std::endl;
+    std::cout << "Attributes = " << std::hex << csbi.wAttributes << std::dec << std::endl;
+    std::cout << "Window upper corner = " << csbi.srWindow.Left << "," << csbi.srWindow.Top << std::endl;
+    std::cout << "Window lower corner = " << csbi.srWindow.Right << "," << csbi.srWindow.Bottom << std::endl;
+    std::cout << "Maximum number of columns = " << csbi.dwMaximumWindowSize.X << std::endl;
+    std::cout << "Maximum number of rows = " << csbi.dwMaximumWindowSize.Y << std::endl << std::endl;
+    return 0;
+
+}
+
 
 
 
 int main()
 {
-    bufferCreationActivation();
+    OutOnScreen();
 }
 
