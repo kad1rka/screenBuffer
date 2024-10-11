d6ad1173de1850465e36ce489371393dd4593c8b
Nikita Rubtsov <118419926+kad1rka@users.noreply.github.com>
2024-10-11 09:48:49 +0300

    add newScreenBuffer function

--- screenBuffer/screenBuffer.cpp
+++ screenBuffer/screenBuffer.cpp
@@ -85,11 +85,33 @@ int OutOnScreen() {
 
 }
 
+int newScreenBuffer() {
 
+    // 3. Установка новых размеров буфера экрана
+
+    COORD coord;
+    HANDLE hStdOut;
+
+    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    std::cout << "Enter new screen buffer size." << std::endl;
+    std::cout << "A number of columns: ";
+    std::cin >> coord.X;
+    std::cout << "A number of rows: ";
+    std::cin >> coord.Y;
+
+    if (!SetConsoleScreenBufferSize(hStdOut, coord))
+    {
+        std::cout << "Set console screen buffer size failed." << std::endl;
+        return GetLastError();
+
+    }
+    return 0;
+}
 
 
 int main()
 {
-    OutOnScreen();
+    newScreenBuffer();
 }
 
